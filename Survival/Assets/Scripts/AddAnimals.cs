40bc65e6a8d25ba684060d5ae6596649608b276d
SupermanS101 <62188988+SupermanS101@users.noreply.github.com>
2021-03-30 13:26:12 -0400

    co,,ent

--- Survival/Assets/Scripts/AddAnimals.cs
+++ Survival/Assets/Scripts/AddAnimals.cs
@@ -16,10 +16,14 @@ public class AddAnimals : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If pressing B and rabbit count < 5
         if (Input.GetKeyDown(KeyCode.B) && counter < 5)
         {
+            //Random position in 35 unit sphere. Always spawns from middle
             Vector3 position = Random.insideUnitSphere * 35;
+            //New rabbit object is instnatiated at that position
             GameObject newRabbit = Instantiate(rabbit, new Vector3(position.x, 0.2f, position.y), Quaternion.identity) as GameObject;
+            //Scaling down the rabbit's size
             newRabbit.transform.localScale = new Vector3(7.5f, 7.5f, 7.5f);
             counter++;
             Debug.Log(counter);
