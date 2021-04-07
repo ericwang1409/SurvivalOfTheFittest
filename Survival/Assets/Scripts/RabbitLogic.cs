767f1e3a7eb26e2bad2a4c3bff44ffaa971dd239
SleevelessMicrowave <62040052+SleevelessMicrowave@users.noreply.github.com>
2021-04-06 21:05:37 -0400

    grass tag

--- Survival/Assets/Scripts/RabbitLogic.cs
+++ Survival/Assets/Scripts/RabbitLogic.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class RabbitLogic : MonoBehaviour
 {
+    private float hunger;
+    private float thirst;
+
+    public float diminishingTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hunger = 100;
+        thirst = 100;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        hunger -= diminishingTime * Time.deltaTime;
+        thirst -= diminishingTime * Time.deltaTime;
+
+        
+    }
+
+    void FindFood()
     {
         
     }
